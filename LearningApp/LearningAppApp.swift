da2de2da15d51a2307897c421e81c0fbc8b04806
Lea <lea.dukaez@gmail.com>
2021-08-25 11:17:03 +0200

    Initial Commit

---
+++ LearningApp/LearningAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  LearningAppApp.swift
+//  LearningApp
+//
+//  Created by Léa Dukaez on 25/08/2021.
+//
+
+import SwiftUI
+
+@main
+struct LearningAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
